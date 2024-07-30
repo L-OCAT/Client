5a636e956d7c7c55c7edff8839dff3c61ba97a5b
crucial-sub <jssub940@gmail.com>
2024-07-30 19:33:35 +0900

    feat: 바텀탭 구현

---
+++ src/lib/utils/bottom-tab-helper.ts
@@ -0,0 +1,55 @@
+import type {BottomTabNavigationOptions} from '@react-navigation/bottom-tabs';
+import HomeIcon from '../../assets/svg/ico_home.svg';
+import MapIcon from '../../assets/svg/ico_map.svg';
+import MyPageIcon from '../../assets/svg/ico_my_page.svg';
+import {BottomTabParamList} from '../../navigation/types';
+import {COLORS, FONTFAMILY} from '../styles/theme';
+
+const defaultOptions: BottomTabNavigationOptions = {
+  tabBarActiveTintColor: COLORS.orange.Orange01,
+  tabBarInactiveTintColor: COLORS.gray.Gray04,
+};
+
+export const getBottomTabOptions = (
+  routeName: keyof BottomTabParamList,
+): BottomTabNavigationOptions => {
+  const options: BottomTabNavigationOptions = defaultOptions;
+
+  switch (routeName) {
+    case 'HomeTab':
+      options.tabBarLabel = '홈';
+      options.tabBarLabelStyle = {
+        fontSize: 13,
+        fontFamily: FONTFAMILY.pretendard_bold,
+      };
+      options.tabBarIcon = props =>
+        HomeIcon({
+          ...props,
+          fill: props.color,
+          width: 28,
+          height: 28,
+        });
+      break;
+    case 'MapTab':
+      options.tabBarLabel = '지도';
+      options.tabBarIcon = props =>
+        MapIcon({
+          ...props,
+          fill: props.color,
+          width: 28,
+          height: 28,
+        });
+      break;
+    case 'MyPageTab':
+      options.tabBarLabel = '마이페이지';
+      options.tabBarIcon = props =>
+        MyPageIcon({
+          ...props,
+          fill: props.color,
+          width: 28,
+          height: 28,
+        });
+  }
+
+  return options;
+};
--- src/navigation/bottomTab/BottomTabNavigator.tsx
+++ src/navigation/bottomTab/BottomTabNavigator.tsx
@@ -1,5 +1,8 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import {StyleSheet} from 'react-native';
+import {COLORS} from '../../lib/styles/theme';
+import {getBottomTabOptions} from '../../lib/utils/bottom-tab-helper';
 import HomeStackNavigator from './HomeStackNavigator';
 import MapStackNavigator from './MapStackNavigator';
 import MyPageStackNavigator from './MyPageStackNavigator';
@@ -9,13 +12,39 @@ const BottomTab = createBottomTabNavigator();
 const BottomTabNavigator = () => {
   return (
     <BottomTab.Navigator
-      screenOptions={{headerShown: false}}
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: styles.TabBarStyle,
+      }}
       initialRouteName="HomeTab">
-      <BottomTab.Screen name="HomeTab" component={HomeStackNavigator} />
-      <BottomTab.Screen name="MapTab" component={MapStackNavigator} />
-      <BottomTab.Screen name="MyPageTab" component={MyPageStackNavigator} />
+      <BottomTab.Screen
+        options={({route}) => getBottomTabOptions(route.name)}
+        name="HomeTab"
+        component={HomeStackNavigator}
+      />
+      <BottomTab.Screen
+        options={({route}) => getBottomTabOptions(route.name)}
+        name="MapTab"
+        component={MapStackNavigator}
+      />
+      <BottomTab.Screen
+        options={({route}) => getBottomTabOptions(route.name)}
+        name="MyPageTab"
+        component={MyPageStackNavigator}
+      />
     </BottomTab.Navigator>
   );
 };
 
 export default BottomTabNavigator;
+
+const styles = StyleSheet.create({
+  TabBarStyle: {
+    height: 80,
+    position: 'absolute',
+    borderTopWidth: 1,
+    borderTopColor: COLORS.shadow,
+    borderRadius: 8,
+  },
+});
