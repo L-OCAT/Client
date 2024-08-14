a01f5c08606405072fe331c3f5ae23b3bdeb2f83
crucial-sub <jssub940@gmail.com>
2024-08-14 19:21:19 +0900

    fix: 범용 스케일팩터 수식 변경 및 오타 수정

--- src/lib/utils/dimensions.ts
+++ src/lib/utils/dimensions.ts
@@ -4,19 +4,17 @@ export const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} =
   Dimensions.get('window');
 
 // iPhone 14 해상도
-const BASE_WIDHT = 390;
+const BASE_WIDTH = 390;
 const BASE_HEIGHT = 844;
 
-const scaleWidth = SCREEN_WIDTH / BASE_WIDHT;
-const scaleHeight = SCREEN_HEIGHT / BASE_HEIGHT;
+const widthRatio = SCREEN_WIDTH / BASE_WIDTH;
+const heightRatio = SCREEN_HEIGHT / BASE_HEIGHT;
 
-//둘 중 더 작은 스케일 팩터를 사용하여 혹시모를 오버플로우 방지
-export const scale = Math.min(scaleWidth, scaleHeight);
+const widthScale = (size: number) => Math.round(size * widthRatio);
+const heightScale = (size: number) => Math.round(size * heightRatio);
+const moderateScale = (size: number, factor = 1) =>
+  size + (widthScale(size) - size) * factor;
 
-const scaledSize = (size: number) => Math.round(size * scale);
-const scaledWidth = (size: number) => Math.round(size * scaleWidth);
-const scaledHeight = (size: number) => Math.round(size * scaleHeight);
-
-export const s = scaledSize;
-export const w = scaledWidth;
-export const h = scaledHeight;
+export const ws = widthScale;
+export const hs = heightScale;
+export const ms = moderateScale;
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -8,7 +8,7 @@ import {typography} from '../../lib/styles/typography';
 import FastImage from 'react-native-fast-image';
 import RightIcon from '../../assets/svg/icon_right.svg';
 import {useScreenLayout} from '../../hooks/useScreenLayout';
-import {h, s, w} from '../../lib/utils/dimensions';
+import {ms} from '../../lib/utils/dimensions';
 import {
   BottomTabParamList,
   MainStackNavigationProp,
@@ -110,25 +110,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    gap: h(50),
+    gap: ms(50),
     backgroundColor: COLORS.orange.Orange01,
   },
   titleText: {
     width: '100%',
     color: COLORS.white,
-    marginTop: h(112),
-    paddingHorizontal: w(16),
+    marginTop: ms(112),
+    paddingHorizontal: ms(16),
   },
   characterImage: {
     position: 'absolute',
-    left: w(225),
-    top: h(64),
-    width: w(245),
-    height: h(279),
+    left: ms(225),
+    top: ms(64),
+    width: ms(245),
+    height: ms(279),
   },
   contentsWrapper: {
     position: 'absolute',
-    top: h(249),
+    top: ms(249),
     bottom: 0,
     left: 0,
     right: 0,
@@ -137,10 +137,10 @@ const styles = StyleSheet.create({
     borderTopEndRadius: 20,
   },
   navigationBtnWrapper: {
-    paddingTop: h(20 + 12),
-    paddingBottom: h(20),
-    paddingHorizontal: w(16),
-    gap: h(10),
+    paddingTop: ms(20 + 12),
+    paddingBottom: ms(20),
+    paddingHorizontal: ms(16),
+    gap: ms(10),
   },
   registrationBtnWrapper: {
     width: '100%',
@@ -152,54 +152,54 @@ const styles = StyleSheet.create({
     borderRadius: 12,
   },
   registrationBtn: {
-    width: w(166),
-    height: h(180),
-    padding: s(16),
+    width: ms(166),
+    height: ms(180),
+    padding: ms(16),
     justifyContent: 'space-between',
   },
   lostItemImage: {
-    width: w(77),
-    height: h(92),
+    width: ms(77),
+    height: ms(92),
     alignSelf: 'flex-end',
   },
   foundItemImage: {
-    width: w(87),
-    height: h(88),
+    width: ms(87),
+    height: ms(88),
     alignSelf: 'flex-end',
   },
   matchingListBtn: {
-    width: w(343),
-    height: h(76),
-    padding: s(16),
+    width: ms(343),
+    height: ms(76),
+    padding: ms(16),
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
   matchingListImage: {
-    width: w(40),
-    height: h(40),
+    width: ms(40),
+    height: ms(40),
   },
   bottomContentsWrapper: {
-    paddingVertical: h(20),
-    paddingHorizontal: w(16),
-    gap: h(16),
+    paddingVertical: ms(20),
+    paddingHorizontal: ms(16),
+    gap: ms(16),
   },
   subText: {
     fontFamily: FONTFAMILY.pretendard_medium,
-    fontSize: 16,
+    fontSize: ms(16, 0.1),
     lineHeight: 16 * 1.3,
     color: COLORS.gray.Gray04,
   },
   bottomContentsBtn: {
     justifyContent: 'center',
-    height: h(100),
+    height: ms(100),
   },
   iconBox: {
     position: 'absolute',
     justifyContent: 'center',
     alignItems: 'center',
-    left: w(295),
-    width: w(48),
-    height: h(48),
+    left: ms(295),
+    width: ms(48),
+    height: ms(48),
   },
 });
