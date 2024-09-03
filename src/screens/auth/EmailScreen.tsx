6f720f97668a481a2d3293c36e1f6cf5110d350e
Soo Jung <comme746@naver.com>
2024-09-03 10:29:29 +0900

    getErrorMessage 함수 작성 / 기능별로 함수 분리

--- src/screens/auth/EmailScreen.tsx
+++ src/screens/auth/EmailScreen.tsx
@@ -19,52 +19,49 @@ import { useTimer, useFormatTime } from '../../hooks/useTimer';
 
 const INITIAL_TIME = 179;
 
+enum ErrorType {
+  EMAIL_INVALID,
+  EMAIL_DUPLICATE,
+  AUTH_NUMBER_INVALID,
+  AUTH_NUMBER_TIMEOUT,
+}
+
 const EmailScreen = () => {
   const navigation = useNavigation<AuthStackNavigationProp>();
   const screenLayout = useScreenLayout();
   const [email, setEmail] = useState('');
   const [authNumber, setAuthNumber] = useState('');
-  const [showEmailValidErrorMessage, setShowEmailValidErrorMessage] = useState(false);
-  const [showEmailDuplicateErrorMessage, setShowEmailDuplicateErrorMessage] = useState(false);
   const [isAuthNumberValid, setIsAuthNumberValid] = useState(false);
-  const [showAuthNumberErrorMessage, setShowAuthNumberErrorMessage] = useState(false);
   const [isCreateAuthNumber, setIsCreateAuthNumber] = useState(false)
-  
+  const [error, setError] = useState<ErrorType | null>(null);
+
   const { remainingTime, resetTimer } = useTimer(INITIAL_TIME);
   const formatTime = useFormatTime();
 
-  const handleResendClick = () => {
-    resetTimer();
+  const getErrorMessage = (errorType: ErrorType | null): string => {
+    switch (errorType) {
+      case ErrorType.EMAIL_INVALID:
+        return '이메일 형식을 확인해주세요';
+      case ErrorType.EMAIL_DUPLICATE:
+        return '이미 사용중인 이메일입니다';
+      case ErrorType.AUTH_NUMBER_INVALID:
+        return '잘못된 인증번호입니다';
+      case ErrorType.AUTH_NUMBER_TIMEOUT:
+        return '입력 시간이 초과됐습니다'
+      default:
+        return '';
+    }
   };
 
+  // 이메일 유효성 검사
   const validateEmail = (value: string) => {
-    //이메일 유효성 검사
     const emailRegex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
     const isValid = emailRegex.test(value);
-    setShowEmailValidErrorMessage(!isValid)
-    
-    //이메일 유효성 검사를 통과하지 못한 경우
-    if (!isValid) {
-      setIsCreateAuthNumber(false);
-      return;
-    };
 
-    // 유효성 검사를 통과한 경우 이메일 중복검사 실행
-    checkEmailDuplicate(value)
-    .then(checkDuplicate => {
-      setShowEmailDuplicateErrorMessage(!checkDuplicate);
-      
-      if (checkDuplicate) {
-        createAuthNumber();
-      }
-    })
-    .catch(error => {
-      console.log('중복 검사 중 에러 발생: ', error)
-      // 오류 메시지 또는 사용자에게 알림 처리
-      setShowEmailDuplicateErrorMessage(true);
-    });
+    return isValid;
   };
-  
+
+  // 이메일 중복 검사
   const checkEmailDuplicate = async (email: string): Promise<boolean> => {
     // 이메일 중복검사 API 호출
     return new Promise((resolve, reject) => {
@@ -72,18 +69,43 @@ const EmailScreen = () => {
       setTimeout(() => resolve(true), 0); //중복검사를 통과했으면 true, 통과하지 못했으면 false
     });
   };
-  
-  // 인증번호 확인
-  const validateAuthNumber = () => {
-    setIsAuthNumberValid(false)
-    setShowAuthNumberErrorMessage(true)
-  }
 
   // 인증번호 생성
   const createAuthNumber = () => {
     setAuthNumber('')
     setIsCreateAuthNumber(true)
-    handleResendClick()
+    setError(null)
+    resetTimer();
+  }
+
+  //남은 시간이 0일시 에러메시지 출력
+  useEffect(()=>{
+    if(remainingTime === 0) {
+      setError(ErrorType.AUTH_NUMBER_TIMEOUT)
+    }
+  },[remainingTime])
+
+  // onPress 이벤트 핸들러
+  const handleSendAuthNumber = async () => {
+    if (!validateEmail(email)) {
+      setError(ErrorType.EMAIL_INVALID);
+      return;
+    }
+
+    const isDuplicate = await checkEmailDuplicate(email);
+    if (!isDuplicate) {
+      setError(ErrorType.EMAIL_DUPLICATE);
+      return;
+    }
+
+    // 모두 통과하면 인증번호 생성
+    createAuthNumber();
+  };
+  
+  // 인증번호 확인
+  const validateAuthNumber = () => {
+    setIsAuthNumberValid(false)
+    setError(ErrorType.AUTH_NUMBER_INVALID);
   }
   
   const handleNavigateToProfileScreen = () => {
@@ -98,7 +120,7 @@ const EmailScreen = () => {
         <View style={styles.textInput}>
           <TextInput
             style={[
-              showEmailDuplicateErrorMessage || showEmailValidErrorMessage ? 
+              (error === ErrorType.EMAIL_INVALID || error === ErrorType.EMAIL_DUPLICATE) ? 
               textInputStyles.errorInput : 
               textInputStyles.default, 
               { flex: 1 },
@@ -109,31 +131,27 @@ const EmailScreen = () => {
             placeholderTextColor="#c1c1c1"
             value={email}
             onChangeText={e => setEmail(e)}
-            onSubmitEditing={() => validateEmail(email)}
+            onSubmitEditing={() => handleSendAuthNumber()}
             editable={!isCreateAuthNumber}
           />
           <TertiaryMediumBtn
             text={isCreateAuthNumber ? '재전송' : '인증전송'}
             onPress={() => {
-              isCreateAuthNumber ? createAuthNumber() : validateEmail(email), 
-              setShowAuthNumberErrorMessage(false)
+              isCreateAuthNumber ? createAuthNumber() : handleSendAuthNumber()
             }}
             isDisabled={!email}
           />
         </View>
-        {showEmailValidErrorMessage &&
-        <Text style={[smallTextStyles.error, styles.smallText]}>
-          올바른 이메일 주소를 입력하세요
-        </Text>}
-        {showEmailDuplicateErrorMessage &&
-        <Text style={[smallTextStyles.error, styles.smallText]}>
-          중복된 이메일 주소입니다
-        </Text>}
+        {(error === ErrorType.EMAIL_INVALID || error === ErrorType.EMAIL_DUPLICATE) && (
+          <Text style={[smallTextStyles.error, styles.smallText]}>
+            {getErrorMessage(error)}
+          </Text>
+        )}
         {isCreateAuthNumber && 
         <View style={styles.textInput}>
           <TextInput
             style={[
-              showAuthNumberErrorMessage ? textInputStyles.errorInput : textInputStyles.default,
+              error === ErrorType.AUTH_NUMBER_INVALID ? textInputStyles.errorInput : textInputStyles.default,
               { flex: 1 },
             ]}
             autoCapitalize="none"
@@ -149,10 +167,11 @@ const EmailScreen = () => {
             isDisabled={!authNumber || remainingTime === 0}
           />
         </View>}
-        {showAuthNumberErrorMessage &&
-        <Text style={[smallTextStyles.error, styles.smallText]}>
-          잘못된 인증번호입니다  
-        </Text>}
+        {(error === ErrorType.AUTH_NUMBER_INVALID || error === ErrorType.AUTH_NUMBER_TIMEOUT) && (
+          <Text style={[smallTextStyles.error, styles.smallText]}>
+            {getErrorMessage(error)}
+          </Text>
+        )}
         {isCreateAuthNumber && 
         <Text style={[smallTextStyles.default, styles.smallText]}>
           남은 시간: {formatTime(remainingTime)}
